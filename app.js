6d07fee2b00a7dcc9e62141682eb6c10434d6bea
Mukesh Munisubbanna <me@mukeshm.com>
2018-03-12 16:18:52 +0530

    API to shorten a url

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const app = express()
 
+const utils = require('./utils')
 const PORT = process.env.PORT || 8080
+
+app.use(express.json())
 
 app.get('/', function (req, res) {
   res.send('Hello from shorty')
 })
+
+app.post('/shorten', [function (req, res, next) {
+  if ('url' in req.body) {
+    next()
+  } else {
+    res.status(400)
+    res.json({'status': 'failure', 'reason': 'malformed request'})
+  }
+}, function (req, res) {
+  let body = req.body
+  let shortUrl = utils.generateShortUrl(body.url)
+  let payload = {
+    'short_url': shortUrl
+  }
+  let result = {
+    'status': 'success',
+    payload
+  }
+  res.json(result)
+}])
 
 app.use(function (req, res, next) {
   res.status(404).send("Sorry can't find that!")
--- utils.js
+++ utils.js
@@ -1,7 +1,13 @@
 const uuidv4 = require('uuid/v4')
+const DOMAIN = process.env.domain || 'localhost:8080'
 
 function generateCode () {
   return uuidv4()
 }
 
-exports.generateCode = generateCode
+function generateShortUrl (url) {
+  console.log(generateCode())
+  console.log(DOMAIN)
+  return url
+}
+exports.generateShortUrl = generateShortUrl
